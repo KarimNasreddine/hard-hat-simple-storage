ff0da91c9e50e4fcb4386bc50a5fcce138c0c3ce
KarimNasreddine <abedkarimnasreddine@gmail.com>
2023-08-31 11:02:13 +0300

    First Commit

---
+++ test/test-deploy.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+const { expect, assert } = require("chai");
+
+describe("SimpleStorage", function () {
+  let SimpleStorageFactory, SimpleStorage;
+  beforeEach(async function () {
+    SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    SimpleStorage = await SimpleStorageFactory.deploy();
+  });
+
+  it("Should start with a favorite number of 0", async function () {
+    const currentValue = await SimpleStorage.retrieve();
+    const expectedValue = "0";
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+
+  it("Should update when we call store", async function () {
+    expectedValue = "7";
+    const transactionResponse = await SimpleStorage.store(expectedValue);
+    await transactionResponse.wait(1);
+
+    const currentValue = await SimpleStorage.retrieve();
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+});
